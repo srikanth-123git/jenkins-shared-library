a4583175ec4de654ce4daace32c329864bf37121
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-06-30 22:34:13 +0530

    adding code checkout option

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -1,11 +1,27 @@
 def call(){
   node('ci-server') {
+
+    stage('CodeCheckout') {
+
+      sh "find ."
+      sh "find . | sed -e '1d' |xargs rm -rf"
+      if(env.TAG_NAME ==~ ".*") {
+        env.branch_name = "refs/tags/${env.TAG_NAME}"
+      } else {
+        env.branch_name = "${env.BRANCH_NAME}"
+      }
+      checkout scmGit(
+          branches: [[name: "${branch_name}"]],
+          userRemoteConfigs: [[url: "https://github.com/srikanth-123git/expense-${component}"]]
+      )
+    }
     if (env.TAG_NAME ==~ '.*') {
       stage('Build Code') {
-        print 'OK'
+        sh 'docker build -t 471112738465.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} .'
       }
       stage('Release Software') {
-        print 'OK'
+        sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 471112738465.dkr.ecr.us-east-1.amazonaws.com'
+        sh 'docker push 471112738465.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
       }
     } else {
       stage('Lint Code') {
