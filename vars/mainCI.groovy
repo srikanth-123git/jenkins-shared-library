43077e566247bdca9ec3df7140415e72bb026444
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-27 11:13:11 +0530

    update CI pipeline

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -12,16 +12,16 @@ def call(){
       }
       checkout scmGit(
           branches: [[name: "${branch_name}"]],
-          userRemoteConfigs: [[url: "https://github.com/srikanth-123git/expense-${component}"]]
+          userRemoteConfigs: [[url: "https://github.com/srikanth-123git/roboshop-${component}"]]
       )
     }
     if (env.TAG_NAME ==~ '.*') {
       stage('Build Code') {
-        sh 'docker build -t 471112738465.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} .'
+        sh 'docker build -t 471112738465.dkr.ecr.us-east-1.amazonaws.com/roboshop-${component}:${TAG_NAME} .'
       }
       stage('Release Software') {
         sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 471112738465.dkr.ecr.us-east-1.amazonaws.com'
-        sh 'docker push 471112738465.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
+        sh 'docker push 471112738465.dkr.ecr.us-east-1.amazonaws.com/roboshop-${component}:${TAG_NAME}'
       }
       stage('Deploy to Dev') {
         sh 'aws eks update-kubeconfig --name dev-eks'
